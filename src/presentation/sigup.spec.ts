bb0333389d22e2826bb734ab50686349c881f87e
acabrala <andre.cagp@gmail.com>
2021-01-05 13:33:24 -0300

    test: ensure SignUpController an error if no name is provided

--- src/presentation/sigup.spec.ts
+++ src/presentation/sigup.spec.ts
@@ -14,6 +14,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
   test('Should returns 400 if no name is provided', () => {
