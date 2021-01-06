db538547837a7cf8fe459cd1d7b09f689cbd133e
acabrala <andre.cagp@gmail.com>
2021-01-05 21:20:37 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/sigup.spec.ts
+++ src/presentation/controllers/signup/sigup.spec.ts
@@ -238,4 +238,27 @@ describe('SignUp Controller', () => {
     expect(httpResponse?.statusCode).toBe(500)
     expect(httpResponse?.body).toEqual(new ServerError())
   })
+
+  test('Should returns 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+        phone: '5511957992969'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(200)
+    expect(httpResponse?.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_mail@mail.com',
+      password: 'valid_password',
+      phone: 'valid_phone'
+    })
+  })
 })
